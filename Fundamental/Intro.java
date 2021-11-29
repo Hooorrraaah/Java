63bfa0e71e7ab5a17edb9c13fd1624864e36f688
Hooorrraaah <austen.jensen@ymail.com>
2021-11-29 14:25:45 -0800

    initial commit

---
+++ Fundamental/Intro.java
@@ -0,0 +1,7 @@
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("My name is Austen Jensen");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Broken Arrow, Ok");
+    }
+}
