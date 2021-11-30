c4b89449ab0813366986b17b8c31d04a3160f8a2
Hooorrraaah <austen.jensen@ymail.com>
2021-11-29 21:59:30 -0800

    intial commit

---
+++ Fundamentals/cafeBusiness/CafeUtil.java
@@ -0,0 +1,16 @@
+public class CafeUtil{
+    public Integer getStreakGoal(){
+        int sum = 0;
+        for(int i=1;i<=10;i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+//     public Integer getOrderTotal(){
+//         for(int i=[0];i<5;i++)
+
+//         return 
+//     }
+
+
+}
